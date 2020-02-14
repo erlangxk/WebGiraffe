f5a48a920e35a418980aece90fc54399523aeb33
erlangxk <simon.xikun@gmail.com>
2020-02-14 11:07:59 +0800

    use giraffe framework

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,15 +1,7 @@
 namespace Server
 
-open System
-open System.Collections.Generic
-open System.IO
-open System.Linq
-open System.Threading.Tasks
-open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
 
 module Program =
     let exitCode = 0
@@ -17,7 +9,7 @@ module Program =
     let CreateHostBuilder args =
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder.UseStartup<Startup>() |> ignore
+                webBuilder.UseStartup<Init.Startup>() |> ignore
             )
 
     [<EntryPoint>]
--- src/Server/Startup.fs
+++ src/Server/Startup.fs
@@ -1,26 +1,21 @@
 namespace Server
 
-open System
+
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Hosting
+open Giraffe
 
-type Startup() =
+module WebRoute =
+    let webApp =
+        choose [
+            route "/ping" >=> text "pong"
+        ]
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    member this.ConfigureServices(services: IServiceCollection) =
-        ()
+module Init =
+    type Startup() =
+        member __.ConfigureServices(services: IServiceCollection) =
+            services.AddGiraffe() |> ignore
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
-        if env.IsDevelopment() then
-            app.UseDeveloperExceptionPage() |> ignore
-
-        app.UseRouting() |> ignore
-
-        app.UseEndpoints(fun endpoints ->
-            endpoints.MapGet("/", fun context -> context.Response.WriteAsync("Hello World!")) |> ignore
-            ) |> ignore
+        member __.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
+            app.UseGiraffe WebRoute.webApp
