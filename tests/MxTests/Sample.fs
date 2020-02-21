0825c31e46273c61ea8edde189fbd020da1ec1f6
erlangxk <simon.xikun@gmail.com>
2020-02-21 11:12:51 +0800

    try Json lib in test

--- tests/MxTests/Sample.fs
+++ tests/MxTests/Sample.fs
@@ -1,6 +1,13 @@
 module Tests
 
 open Expecto
+open Microsoft.FSharpLu.Json
+
+
+type Person = 
+  { Username: string
+    Password: string}
+
 
 [<Tests>]
 let tests =
@@ -8,4 +15,19 @@ let tests =
     testCase "universe exists" <| fun _ ->
       let subject = false
       Expect.isTrue subject "I compute, therefore I am."
+    
+    testCase "json" <| fun _ ->
+      let a = { Username= "aaa"; Password= "bbb";}
+      let s = Compact.serialize a
+      printfn "%s" s
+      Expect.isTrue true "OK"
+
+    testCase "read json string" <| fun _ ->
+      let s = """{ "Username": "aaa",  "Password": "bbb"} """
+      let a = Compact.tryDeserialize<Person> s
+      match a with 
+      | Choice1Of2 p -> printf "person %A" p
+      | Choice2Of2 e -> printf "shit %s" e
+      Expect.isTrue true "OK"
+
   ]
