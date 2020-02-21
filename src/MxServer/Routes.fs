3d0c548ce62d546c91510b8243c6f69569ab2757
erlangxk <simon.xikun@gmail.com>
2020-02-21 11:13:16 +0800

    use task CE to check json decode error

--- src/MxServer/Routes.fs
+++ src/MxServer/Routes.fs
@@ -1,38 +1,36 @@
 namespace MxServer
 
+
+open FSharp.Control.Tasks.V2
+open Microsoft.AspNetCore.Http
 open Giraffe
-open FSharpPlus
-
-
-[<CLIMutable>]
-type Credential =
-    { UserName: string option
-      Password: string option }
-
-    static member Check(c: Credential) =
-        let checkName (name: string) =
-            let l = name.Length
-            l >= 1 && l <= 64
-
-        let checkPassword (pwd: string) =
-            let l = pwd.Length
-            l >= 1 && l <= 64
-
-        monad {
-            let! name = c.UserName
-            let! pwd = c.Password
-            if (checkName (name) && checkPassword (pwd))
-            then return (name, pwd)
-            else return! None
-        }
-
 
 module Handlers =
+    [<CLIMutable>]
+    type Credential =
+        { Username: string
+          Password: string }
+
+        static member Check(c: Credential) =
+                let l = c.Username.Length
+                if(l >= 1 && l <= 64) 
+                then 
+                    let l = c.Password.Length
+                    l >= 1 && l <= 64
+                else false
+
+    
     let register: HttpHandler =
-        bindJson<Credential> (fun c ->
-            let greeting =
-                match Credential.Check(c)  with
-                | Some (name,_) -> sprintf "Hello World, from %s" name
-                | None -> sprintf "sorry"
-            text greeting)
+        fun (next:HttpFunc) (ctx:HttpContext) ->
+            task {
+                    try
+                        let! c = ctx.BindJsonAsync<Credential>()
+                        let greeting = 
+                            if(Credential.Check(c))
+                            then  sprintf "Hello World, from %s" c.Username
+                            else sprintf "sorry"
+                        return! text greeting next ctx
+                    with
+                        | ex-> return! RequestErrors.BAD_REQUEST ex next ctx
+            }
 
